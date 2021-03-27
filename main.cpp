98ce2fbb65a9c9b20d0ed7e49bd09e7e665c34de
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2021-03-27 19:09:36 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"**********Menu**********"<<endl<<endl;
+    cout<<"Arrays"<<endl;
+    cout<<"1. Reverse the array"<<endl;
+    int n;
+    cout<<"Enter your choice : ";
+    cin>>n;
+    switch(n)
+    {
+        case 1      :
+                        break;
+
+        default     :
+                        break;
+    }
+    return 0;
+}
