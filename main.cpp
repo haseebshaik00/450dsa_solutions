ee768456203c56c6a54140c9b72eca1420f19dca
Shaik Haseeb Ur Rahman <haseebshaik00@gmail.com>
2021-04-09 01:55:32 +0530

    3rd ques done

--- main.cpp
+++ main.cpp
@@ -8,12 +8,57 @@ string reverseWord(string str){
     return str;
 }
 
+void swapit(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int randomPartition(int arr[], int l, int r)
+{
+        int n = r-l+1;
+        int pivot = rand() % n;
+        swapit(&arr[l + pivot], &arr[r]);
+        return partition(arr, l, r);
+}
+
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    if (k > 0 && k <= r - l + 1)
+    {
+        int pos = randomPartition(arr, l, r);
+        if (pos-l == k-1)
+            return arr[pos];
+        if (pos-l > k-1)
+            return kthSmallest(arr, l, pos-1, k);
+        return kthSmallest(arr, pos+1, r, k-pos+l-1);
+    }
+    return INT_MAX;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int x = arr[r], i = l;
+    for (int j = l; j <= r - 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            swapit(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swapit(&arr[i], &arr[r]);
+    return i;
+}
+
 int main()
 {
     cout<<"**********Menu**********"<<endl<<endl;
     cout<<"Arrays"<<endl;
     cout<<"1. Reverse the array or string"<<endl;
     cout<<"2. Find the maximum and minimum element in an array"<<endl;
+    cout<<"3. Find the kth max and min element in an array - Quick Start Algorithm"<<endl;
     int n;
     cout<<endl<<"Enter your choice : ";
     cin>>n;
@@ -40,6 +85,17 @@ int main()
                                 max_ans = a[i];
                         }
                         cout<<min_ans<<" "<<max_ans<<endl;
+                        break;
+                    }
+        case 3 :    {
+                        int n;
+                        cin>>n;
+                        int a[n];
+                        for(int i=0;i<n;i++)
+                            cin>>a[i];
+                        int k;
+                        cin>>k;
+                        cout<<(a,0,n-1,k)<<endl;
                         break;
                     }
         default  :      cout<<"Try Again!"<<endl;
